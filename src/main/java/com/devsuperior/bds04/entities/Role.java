1c0c917ccf2b2648c74d3681c63713be13e782bb
Thundasantana <ednilson.sdas@outlook.com>
2023-12-07 20:41:31 -0300

    Implements User

--- src/main/java/com/devsuperior/bds04/entities/Role.java
+++ src/main/java/com/devsuperior/bds04/entities/Role.java
@@ -13,12 +13,12 @@ import javax.persistence.Table;
 @Table(name = "tb_role")
 public class Role implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String authority;
-	
+
 	public Role() {
 	}
 
